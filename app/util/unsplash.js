6bbf148485b96b0b126dd78cf7fa2d361d40ef91
Gosha Arinich <me@goshakkk.name>
2016-06-30 13:20:39 +0300

    Add a few more images

--- app/util/unsplash.js
+++ app/util/unsplash.js
@@ -1,6 +1,9 @@
 // TODO: query a real API
 
 const images = [
+  { url: 'https://images.unsplash.com/photo-1461016951828-c09537329b3a?fm=jpg', tags: ['field', 'landscape', 'sunlight'] },
+  { url: 'https://images.unsplash.com/photo-1461295025362-7547f63dbaea?fm=jpg', tags: ['crops'] },
+  { url: 'https://images.unsplash.com/photo-1465326117523-6450112b60b2?fm=jpg', tags: ['forest', 'hill'] },
   { url: 'https://images.unsplash.com/photo-1458640904116-093b74971de9?fm=jpg', tags: ['dark', 'field'] },
   //{ url: 'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?fm=jpg' },
   //{ url: 'https://images.unsplash.com/photo-1451906278231-17b8ff0a8880?fm=jpg' },
